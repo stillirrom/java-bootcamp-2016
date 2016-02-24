210824acc3ef9559e3873a877a284331827d6139
StillIrrom <irrom@hotmail.com>
2016-02-24 13:54:23 -0500

    UpdateTypo

--- Topics/Topic_0/excersice3/DBAccessor.java
+++ Topics/Topic_0/excersice3/DBAccessor.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class DBAccessor {
 
 	public void sayHello() {
-		System.out.println(this.getClass().getSimpleName() + " established the connection at " + new Date());
+		System.out.println(this.getClass().getSimpleName() + " connection established at " + new Date());
 	}
 
 }
