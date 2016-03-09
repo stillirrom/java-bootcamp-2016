f56ce2ba45177c78bf5253bbff55eb532db75f30
StillIrrom <irrom@hotmail.com>
2016-03-08 19:30:20 -0500

    Update

--- Topics/Topic_4/excercise2/Main.java
+++
@@ -1,9 +0,0 @@
-package local.maven.tdd.SQLExcercise2;
-
-public class Main {
-	public static void main(String[] args) throws Exception {
-		MySQLAccess dao = new MySQLAccess();
-		dao.readDataBase();
-	}
-
-}
